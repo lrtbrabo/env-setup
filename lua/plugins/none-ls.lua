4ab9b378aafdad1d8da1cbc4bb5383e451253f91
Lucas Brabo <lucas.brabo@stefanini.com>
2024-04-09 18:50:09 -0300

    fix bugs

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,10 +6,10 @@ return {
 			sources = {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.goimports_reviser,
+        null_ls.builtins.formatting.gofmt,
+				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.formatting.golines,
-				null_ls.builtins.diagnostics.golangci_lint,
-				null_ls.builtins.diagnostics.eslint_d,
+        null_ls.builtins.completion.spell,
 			},
 		})
     vim.g.nonels_suppress_issue58 = true
--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,4 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set number")
+vim.cmd("set clipboard+=unnamedplus")
 vim.g.mapleader = " "
+vim.opt.clipboard:append("unnamedplus")
